3084e406c0ff8ae5fa8fbbb492478f213c9cf494
maxflores1 <118218453+maxflores1@users.noreply.github.com>
2022-12-02 12:51:14 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+scene.setBackgroundColor(6)
+effects.confetti.startScreenEffect()
+pause(3000)
+game.setDialogFrame(img`
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+    `)
+game.setDialogTextColor(0)
+game.splash("Welcome to good burger")
+effects.confetti.endScreenEffect()
+music.pewPew.playUntilDone()
